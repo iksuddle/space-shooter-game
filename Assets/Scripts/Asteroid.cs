98c0dc358897209cf341b37a40148059b340f416
MahadAnsari <113386055+MahadAnsari@users.noreply.github.com>
2022-11-09 23:34:31 -0500

    Add files via upload

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Asteroid : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody { get; private set; }
+    public SpriteRenderer spriteRenderer { get; private set; }
+    public Sprite[] sprites;
+
+    public float size = 1f;
+    public float minSize = 0.35f;
+    public float maxSize = 1.65f;
+    public float movementSpeed = 50f;
+    public float maxLifetime = 30f;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+        transform.eulerAngles = new Vector3(0f, 0f, Random.value * 360f);
+
+        transform.localScale = Vector3.one * size;
+        rigidbody.mass = size;
+
+        Destroy(gameObject, maxLifetime);
+    }
+
+    public void SetTrajectory(Vector2 direction)
+    {
+        rigidbody.AddForce(direction * movementSpeed);
+    }
+}
