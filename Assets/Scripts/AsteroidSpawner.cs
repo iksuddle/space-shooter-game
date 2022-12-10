6037fb543fe390401fa595044cf576c8acd45945
Ismail Suddle <iksuddle@Ismails-MacBook-Air.local>
2022-12-10 15:39:33 -0500

    finished level 1

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+public class AsteroidSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject asteroidPrefab;
     [Space]
