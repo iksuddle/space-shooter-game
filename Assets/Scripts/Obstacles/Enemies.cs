c3d4c44f289b90971f19d169de62bc650eea1337
MahadAnsari <113386055+MahadAnsari@users.noreply.github.com>
2022-12-11 20:45:15 -0500

    Add files via upload

---
+++ Assets/Scripts/Obstacles/Enemies.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Vector2 direction;
+    public float speed;
+    public float shootingDelay;
+    public float lastTimeShot = 0f;
+    public float bulletSpeed;
+    public Transform player;
+    public GameObject bullet;
+
+    void Start() {
+        player=GameObject.FindWithTag ("Player").transform;    
+    }
+
+    void Update() {
+        if (Time.time > lastTimeShot + shootingDelay) {
+            float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg - 90f;
+            Quaternion q = Quaternion.AngleAxis (angle, Vector3.forward);
+
+            GameObject newBullet = Instantiate(bullet, transform.position,q);
+
+            newBullet.GetComponent<Rigidbody2D> ().AddRelativeForce (new Vector2(0f, bulletSpeed));
+            lastTimeShot = Time.time;
+        }
+    }
+
+    void FixedUpdate() {
+        direction = (player.position - transform.position).normalized;
+        rb.MovePosition (rb.position + direction * speed * Time.fixedDeltaTime);
+    }
+}
